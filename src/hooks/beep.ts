79962b45b505ec7062632620c59972c5607c3c15
Dylan Vidal <dylanvidal1204@gmail.com>
2024-07-21 14:46:37 -0400

    Update hooks/beep for testing

--- src/hooks/beep.ts
+++ src/hooks/beep.ts
@@ -3,7 +3,5 @@ import { WebhookClient } from "discord.js";
 // Test webhook command
 export async function execute(webhook: WebhookClient) {
     // Send a hello world message every 10 seconds
-    setInterval(() => {
-        webhook.send("Beep Boop!");
-    }, 10000);
+    webhook.send("Beep Boop!");
 }
