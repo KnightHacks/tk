b2159f1f0a25a5a6ecef496bfc45e2bdf5873c1c
must108 <mustaeen18@gmail.com>
2024-07-21 16:26:58 -0400

    edit joke command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,11 +1,11 @@
 import * as beep from "./beep";
 import * as duck from "./duck";
-import * as programmingjoke from './programmingjoke';
+import * as joke from "./joke";
 
 
 // Export all commands
 export const commands = {
     beep,
     duck,
-    programmingjoke,
+    joke,
 };
