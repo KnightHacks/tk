a3dce2418a4f4f11ce7c1974099e94c0388b7b19
must108 <mustaeen18@gmail.com>
2024-08-02 23:15:56 -0400

    added links command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -13,6 +13,7 @@ import * as help from "./help";
 import * as dog from "./dog";
 import * as capybara from "./capybara";
 import * as goat from "./goat";
+import * as links from "./links";
 
 // Export all commands
 export const commands = {
@@ -31,4 +32,5 @@ export const commands = {
     help,
     capybara,
     goat,
+    links,
 };
